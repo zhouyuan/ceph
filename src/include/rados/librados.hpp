e8463fa39456f289d5574728f79413410752f051
Sage Weil <sage@inktank.com>
2013-10-03 12:38:40 -0700

    librados: stubs to determine hash position when enumerating objects
    Signed-off-by: Sage Weil <sage@inktank.com>

--- src/include/rados/librados.hpp
+++ src/include/rados/librados.hpp
@@ -71,6 +71,13 @@ namespace librados
     ObjectIterator &operator++(); // Preincrement
     ObjectIterator operator++(int); // Postincrement
     friend class IoCtx;
+
+    /// get current hash position of the iterator
+    uint32_t get_hash_position() const;
+
+    /// get current hash position of the iterator, rounded to the current pg
+    uint32_t get_pg_hash_position() const;
+
   private:
     void get_next();
     std::tr1::shared_ptr < ObjListCtx > ctx;
