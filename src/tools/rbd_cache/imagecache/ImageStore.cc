ed402adfe408881a811d8b839b0095b7317d0764
Yuan Zhou <yuan.zhou@intel.com>
2018-03-12 20:53:39 +0800

    librbd: shared rbd ssd cache draft
    Signed-off-by: Yuan Zhou <yuan.zhou@intel.com>

---
+++ src/tools/rbd_cache/imagecache/ImageStore.cc
@@ -0,0 +1,125 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
+
+#include "ImageStore.h"
+#include "include/buffer.h"
+#include "common/dout.h"
+#include "Types.h"
+
+#define dout_subsys ceph_subsys_rbd
+#undef dout_prefix
+#define dout_prefix *_dout << "librbd::cache::file::ImageStore: " << this \
+                           << " " <<  __func__ << ": "
+
+namespace rbd {
+namespace cache {
+namespace file {
+
+
+ImageStore::ImageStore(CephContext *cct, std::string volume_name, uint64_t image_size)
+  : m_image_size(image_size),
+    m_cache_file(cct, *image_ctx.op_work_queue,
+                 volume_name + ".image_cache") {
+  CephContext *cct = m_image_ctx.cct;
+  ldout(cct, 20) << "volume_name: " << volume_name << dendl;
+}
+
+
+void ImageStore::init(Context *on_finish) {
+  CephContext *cct = m_image_ctx.cct;
+  ldout(cct, 20) << dendl;
+
+  // TODO don't reset the read-only cache
+  Context *ctx = new FunctionContext(
+    [this, on_finish](int r) {
+      if (r < 0) {
+        on_finish->complete(r);
+        return;
+      }
+
+      reset(on_finish);
+    });
+  m_cache_file.open(ctx);
+}
+
+
+void ImageStore::remove(Context *on_finish) {
+  CephContext *cct = m_image_ctx.cct;
+  ldout(cct, 20) << dendl;
+
+  m_cache_file.remove(on_finish);
+}
+
+
+void ImageStore::shut_down(Context *on_finish) {
+  CephContext *cct = m_image_ctx.cct;
+  ldout(cct, 20) << dendl;
+
+  // TODO
+  m_cache_file.close(on_finish);
+}
+
+
+void ImageStore::reset(Context *on_finish) {
+  CephContext *cct = m_image_ctx.cct;
+  ldout(cct, 20) << "Cache_Size: " << m_image_size << dendl;
+
+  // TODO
+  m_cache_file.truncate(m_image_size, false, on_finish);
+}
+
+
+void ImageStore::read_block(uint64_t offset,
+                               BlockExtents &&block_extents,
+                               bufferlist *bl, Context *on_finish) {
+  CephContext *cct = m_image_ctx.cct;
+  /*ldout(cct, 20) << "block=" << cache_block << ", "
+                 << "extents=" << block_extents << dendl;*/
+
+  // TODO add gather support
+  assert(block_extents.size() == 1);
+  auto &extent = block_extents.front();
+  m_cache_file.read(offset + extent.first,
+                    extent.second, bl, on_finish);
+}
+
+
+void ImageStore::write_block(uint64_t offset,
+                                BlockExtents &&block_extents,
+                                bufferlist &&bl, Context *on_finish) {
+  CephContext *cct = m_image_ctx.cct;
+  /*ldout(cct, 20) << "block=" << cache_block << ", "
+                 << "extents=" << block_extents << dendl;*/
+
+  // TODO add scatter support
+  C_Gather *ctx = new C_Gather(cct, on_finish);
+  uint64_t buffer_offset = 0;
+  for (auto &extent : block_extents) {
+    bufferlist sub_bl;
+    sub_bl.substr_of(bl, buffer_offset, extent.second);
+    buffer_offset += extent.second;
+
+    m_cache_file.write(offset + extent.first,
+                       std::move(sub_bl), false, ctx->new_sub());
+
+  }
+  ctx->activate();
+}
+
+
+void ImageStore::discard_block(uint64_t cache_block, Context *on_finish) {
+  CephContext *cct = m_image_ctx.cct;
+  ldout(cct, 20) << dendl;
+
+  // TODO
+  on_finish->complete(0);
+}
+
+
+bool ImageStore::check_exists() {
+    return m_cache_file.try_open();
+}
+
+} // namespace file
+} // namespace cache
+} // namespace rbd
---
+++ src/tools/rbd_cache/imagecache/ImageStore.h
@@ -0,0 +1,49 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
+
+#ifndef CEPH_LIBRBD_CACHE_FILE_IMAGE_STORE
+#define CEPH_LIBRBD_CACHE_FILE_IMAGE_STORE
+
+#include "include/buffer_fwd.h"
+#include "include/int_types.h"
+#include "os/CacheStore/SyncFile.h"
+#include <vector>
+
+struct Context;
+
+namespace rbd {
+
+
+namespace cache {
+namespace file {
+
+class ImageStore {
+public:
+  typedef std::vector<std::pair<uint32_t, uint32_t> > BlockExtents;
+
+  ImageStore(CephContext *cct, std::string volumd_name, uint64_t image_size);
+
+  void init(Context *on_finish);
+  void remove(Context *on_finish);
+  void shut_down(Context *on_finish);
+  void reset(Context *on_finish);
+
+  void read_block(uint64_t offset, BlockExtents &&block_extents,
+                  ceph::bufferlist *bl, Context *on_finish);
+  void write_block(uint64_t offset, BlockExtents &&block_extents,
+                   ceph::bufferlist &&bl, Context *on_finish);
+  void discard_block(uint64_t cache_block, Context *on_finish);
+  bool check_exists();
+
+private:
+  CephContext *m_cct;
+  uint64_t m_image_size;
+  os::CacheStore::SyncFile m_cache_file;
+
+};
+
+} // namespace file
+} // namespace cache
+} // namespace rbd
+
+#endif // CEPH_LIBRBD_CACHE_FILE_IMAGE_STORE
