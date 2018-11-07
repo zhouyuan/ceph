fac67f1e6a8e6fbaa15186bb41489fa580e7d695
Yuan Zhou <yuan.zhou@intel.com>
2018-11-07 13:00:30 +0800

    test: fix tests for sync file
    Signed-off-by: Yuan Zhou <yuan.zhou@intel.com>

--- src/test/ceph_immutable_object_cache/test_sync_file.cc
+++ src/test/ceph_immutable_object_cache/test_sync_file.cc
@@ -1,3 +1,6 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
+
 #include "gtest/gtest.h"
 #include "include/Context.h"
 #include "include/buffer_fwd.h"
@@ -6,42 +9,55 @@
 #include "global/global_init.h"
 #include "common/ceph_argparse.h"
 #include "global/global_context.h"
+#include <experimental/filesystem>
 
 #include "librbd/cache/SharedPersistentObjectCacherFile.h"
 
 using namespace librbd::cache;
+namespace efs = std::experimental::filesystem;
 
-int clean_file(std::string file) {
-  
-  std::string cache_file(g_ceph_context->_conf.get_val<std::string>("rbd_shared_cache_path") 
-    + "/ceph_immutable_obj_cache/" + file);
-   
-  return std::remove(cache_file.c_str());
-}
+class TestSyncFile :public ::testing::Test {
+public:
+  std::string m_cache_root_dir;
 
-TEST(TestSyncFile, test_create_file) {
-  clean_file("test_sync_file");
+  TestSyncFile(){}
+  ~TestSyncFile(){}
+  static void SetUpTestCase() {}
+  static void TearDownTestCase() {}
+
+  void SetUp() override {
+    m_cache_root_dir = g_ceph_context->_conf.get_val<std::string>("rbd_shared_cache_path") 
+      + "/ceph_immutable_obj_cache/";
+
+    if (efs::exists(m_cache_root_dir)) {
+      efs::remove_all(m_cache_root_dir);
+    }
+    efs::create_directories(m_cache_root_dir);
+  }
+
+  void TearDown() override {
+    efs::remove_all(m_cache_root_dir);
+  }
+
+};
+
+TEST_F(TestSyncFile, test_create_file) {
   SyncFile* m_sync_file = new SyncFile(g_ceph_context, "test_sync_file");
   ASSERT_TRUE(m_sync_file->create() >  0);
   ASSERT_TRUE(m_sync_file->get_file_size() == 0);
   delete m_sync_file;
-  clean_file("test_sync_file");
 }
 
 
-TEST(TestSyncFile, test_open_file) {
-  clean_file("test_sync_file");
+TEST_F(TestSyncFile, test_open_file) {
   SyncFile* m_sync_file = new SyncFile(g_ceph_context, "test_sync_file");
   ASSERT_EQ(m_sync_file->open_file(), -1);
   ASSERT_GT(m_sync_file->create(), 0);
   ASSERT_GT(m_sync_file->open_file(), 0);
   delete m_sync_file; 
-  clean_file("test_sync_file");
 }
 
-TEST(TestSyncFile, test_write_object_to_file) {
-  clean_file("test_sync_file_1");
-  clean_file("test_sync_file_2");
+TEST_F(TestSyncFile, test_write_object_to_file) {
   SyncFile* m_sync_file_1 = new SyncFile(g_ceph_context, "test_sync_file_1");
   SyncFile* m_sync_file_2 = new SyncFile(g_ceph_context, "test_sync_file_2");
   ASSERT_GT(m_sync_file_1->create(), 0);
@@ -60,13 +76,9 @@ TEST(TestSyncFile, test_write_object_to_file) {
   delete m_sync_file_2;
   delete buf_1;
   delete buf_2;
-  clean_file("test_sync_file_1");
-  clean_file("test_sync_file_2");
 }
 
-TEST(TestSyncFile, test_read_object_from_file) {
-  clean_file("test_sync_file_1");
-  clean_file("test_sync_file_2");
+TEST_F(TestSyncFile, test_read_object_from_file) {
   SyncFile* m_sync_file_1 = new SyncFile(g_ceph_context, "test_sync_file_1");
   SyncFile* m_sync_file_2 = new SyncFile(g_ceph_context, "test_sync_file_2");
   bufferlist* buf_1 = new ceph::bufferlist();
@@ -95,6 +107,4 @@ TEST(TestSyncFile, test_read_object_from_file) {
   delete m_sync_file_2;
   delete buf_1;
   delete buf_2;
-  clean_file("test_sync_file_1");
-  clean_file("test_sync_file_2");
 }
