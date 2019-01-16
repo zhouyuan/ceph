7b15c6b4c776aa9ed2e682b92a65d84b2ff3a624
Yuan Zhou <yuan.zhou@intel.com>
2019-01-16 23:38:26 +0800

    common/buffer: adding pread_file method
    pread_file allows to read length of contents at a given offset
    
    using pread_file instead of read_file to reduce extra reads in ObjectCacheFile
    
    Signed-off-by: Yuan Zhou <yuan.zhou@intel.com>

--- src/include/buffer.h
+++ src/include/buffer.h
@@ -1207,6 +1207,7 @@ namespace buffer CEPH_BUFFER_API {
 
     void write_stream(std::ostream &out) const;
     void hexdump(std::ostream &out, bool trailing_newline = true) const;
+    int pread_file(const char *fn, uint64_t off, uint64_t len, std::string *error);
     int read_file(const char *fn, std::string *error);
     ssize_t read_fd(int fd, size_t len);
     int write_file(const char *fn, int mode=0644);
